96b7eb9730376aff6c06835914a28607b146f798
Michal Olah <olahmichal@gmail.com>
2011-02-28 11:24:12 +0100

    updating some page texts

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,7 @@ namespace :deploy do
   desc "Symlink shared resources on each release"
   task :symlink_shared, :roles => :app do
     run "ln -nfs #{shared_path}/config/initializers/mongoid_config.rb #{release_path}/config/initializers/mongoid_config.rb"
+    run "ln -nfs #{shared_path}/public/uploads #{release_path}/public/uploads"
   end
 end
 
