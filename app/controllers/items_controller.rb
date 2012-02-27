04e89b8b75e6ee4098f602638460b74928037a99
Michal Olah <m1k3@Michals-MacBook-Pro.local>
2012-02-27 11:05:33 -0800

    fixing some more stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,21 @@
 # -*- encoding : utf-8 -*-
 class ItemsController < ApplicationController
-  
+
   before_filter :login_required
   before_filter :prepare
+  before_filter :disable_timestamp_recording, :only => [:create, :update, :destroy]
+  after_filter :enable_timestamp_recording, :only => [:create, :update, :destroy]
 
   def new
     @item = @model_class.new
-    
+
     @forms = @item.forms
   end
-  
+
   def create
     collection = current_user.send(@embed.identifier)
     @item = collection.build
-    
+
     @forms = @item.forms
     valid = true
     @forms.each do |form|
@@ -21,22 +23,22 @@ class ItemsController < ApplicationController
       form.process
       valid = false unless form.valid?
     end
-    
+
     if valid
       @item.save
-      current_user.touch unless admin_presence?
+      current_user.save(validate: false)
       redirect_to form_path(@embed.settings[:form_identifier])
     else
       flash.now[:error] = "Prosím vyplnte správne všetky povinné položky formulára."
       render :action => "new"
     end
   end
-  
+
   def edit
     @item = current_user.send(@embed.identifier).find(params[:id])
     @forms = @item.forms
   end
-  
+
   def update
     @item = current_user.send(@embed.identifier).find(params[:id])
     @forms = @item.forms
@@ -48,26 +50,27 @@ class ItemsController < ApplicationController
     end
     if valid
       @item.save(:validate => false)
-      current_user.touch unless admin_presence?
+      current_user.save(validate: false)
       redirect_to form_path(@embed.settings[:form_identifier])
     else
       flash.now[:error] = "Prosím vyplnte správne všetky povinné položky formulára."
       render :action => "edit"
     end
   end
-  
+
   def destroy
     @item = current_user.send(@embed.identifier).find(params[:id])
     @item.destroy
+    current_user.save(validate: false)
     redirect_to form_path(@embed.settings[:form_identifier])
   end
-  
+
   protected
-  
+
   def prepare
     @finder = Forms::FormFinder.instance
     @embed = @finder.embed_for_with_identifier(User, params[:embed_id])
     @model_class = @embed.settings[:model_name].constantize
   end
-  
+
 end
