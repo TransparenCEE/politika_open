840d0b11f58b7902a1ef1fd4217907f9723fc35a
Michal Olah <m1k3@Michals-MacBook-Pro.local>
2012-03-26 18:46:08 -0700

    fixup for http://bugtracker.fair-play.sk/issues/93#note-2

--- lib/forms/fields/boolean_picker.rb
+++ lib/forms/fields/boolean_picker.rb
@@ -7,7 +7,7 @@ module Forms
       end
 
       def render
-        options = options_for_select([[nil, nil], ['Áno', '1'], ['Nie', '0']], form_value)
+        options = options_for_select([['Áno', '1'], ['Nie', '0']], form_value || '0')
         select_tag(input_tag_name, options, settings[:html_options]||{})
       end
 
@@ -24,7 +24,6 @@ module Forms
       end
 
       def formatted_value
-        return 'Nevyplnené' if value.nil?
         value ? 'Áno' : 'Nie'
       end
     end
